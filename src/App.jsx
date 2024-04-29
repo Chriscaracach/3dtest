eba5f3e5f5806e860ddd635ce8365de4acf5b802
Christian Caracach <christiancaracach@gmail.com>
2024-04-29 20:55:03 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import * as THREE from "three";
+import "./App.css";
+import { useRef } from "react";
+import { useEffect } from "react";
+
+function App() {
+  const imageUrl = "src/assets/logo_gs_guido_buffo.png";
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const geometry = new THREE.PlaneGeometry(2, 2);
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load(imageUrl);
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    const mesh = new THREE.Mesh(geometry, material);
+
+    scene.add(mesh);
+
+    camera.position.z = 5;
+
+    const handleOrientation = (event) => {
+      const { beta, gamma } = event;
+
+      // Convertir los ángulos de orientación a radianes
+      const betaRad = (beta * Math.PI) / 180;
+      const gammaRad = (gamma * Math.PI) / 180;
+
+      // Mover el objeto en función de los ángulos de orientación
+      mesh.rotation.x = betaRad;
+      mesh.rotation.y = gammaRad;
+    };
+
+    window.addEventListener("deviceorientation", handleOrientation);
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    return () => {
+      // Limpiar recursos al desmontar el componente
+      geometry.dispose();
+      texture.dispose();
+      material.dispose();
+      scene.remove(mesh);
+      renderer.dispose();
+      window.removeEventListener("deviceorientation", handleOrientation);
+    };
+  }, [imageUrl]);
+
+  return <canvas ref={canvasRef} />;
+}
+
+export default App;
